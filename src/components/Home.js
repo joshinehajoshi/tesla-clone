dce8019326b120f5f9e450205cd8056c585cc92e
Neha Joshi <nehajoshigkp@gmail.com>
2021-09-22 09:04:15 +0530

    sections added

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,41 @@ import Section from './Section';
 function Home() {
     return (
         <Container>
-           <Section />
+           <Section
+                title = "Model S"
+                description = "Order Online for Touchless Delivery"
+                background = "model-s.jpg"
+                leftBtnText = "Custom order"
+                rightBtnText = "Existing Inventory"
+           />
+              <Section
+                title = "Model Y"
+                description = "Order Online for Touchless Delivery"
+                background = "model-y.jpg"
+                leftBtnText = "Custom order"
+                rightBtnText = "Existing inventory"
+           />
+             <Section
+                title = "Model 3"
+                description = "Order Online for Touchless Delivery"
+                background = "model-3.jpg"
+                leftBtnText = "Custom order"
+                rightBtnText = "Existing inventory"
+           />
+              <Section
+                title = "Model X"
+                description = "Order Online for Touchless Delivery"
+                background = "model-x.jpg"
+                leftBtnText = "Custom order"
+                rightBtnText = "Existing inventory"
+           />
+              <Section
+                title = "Lowest Cost Solar Panels in America"
+                description = "Money-back guarantee"
+                background = "solar-panel.jpg"
+                leftBtnText = "Order now"
+                rightBtnText = "Learn more"
+           />
         </Container>
        
     )
--- src/components/Section.js
+++ src/components/Section.js
@@ -2,10 +2,25 @@ import React from 'react';
 import styled  from 'styled-components';
 import Header from './Header';
 
-function Section() {
+function Section(props) {
     return (
-        <Wrap>
-            <Header />
+        <Wrap bgImage={props.background}>
+            <ItemText>
+                <h1>{props.title}</h1>
+                <p>{props.description}</p>
+            </ItemText>
+
+            <Button>
+            <ButtonGroup>
+                <LeftButton>
+                    {props.leftBtnText}
+                </LeftButton>
+                <RightButton>
+                {props.rightBtnText}
+                </RightButton>
+            </ButtonGroup>
+            <DownArrow src="./images/down-arrow.svg" />
+            </Button>
         </Wrap>
     )
 }
@@ -13,9 +28,53 @@ function Section() {
 export default Section;
 
 const Wrap = styled.div`
-    width: 100vw;
+    width: 100%;
     height: 100vh;
     background-size: cover !important;
     background-position: center;
-    background: url('./images/model-s.jpg');
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    background: ${props => `url("/images/${props.bgImage}")`}
 `
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center
+`
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+`
+const LeftButton = styled.div`
+    background: rgba(23,26,32,0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+`
+const RightButton = styled(LeftButton)`
+    background-color: white;
+    opacity: 0.65;
+    color: black;
+
+`
+const DownArrow = styled.img`
+    height: 40px;
+    overflow-x: hidden;
+    animation: animateDown infinite 15s;
+`
+
+const Button = styled.div``
